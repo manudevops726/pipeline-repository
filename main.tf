5a482c6c82a6378682f31c32e8719e492edc2979
manasa <amanasa726@gmail.com>
2025-05-07 14:56:15 +0530

    my first approval

---
+++ main.tf
@@ -0,0 +1,14 @@
+
+
+
+  provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-002f6e91abff6eb96"
+    instance_type = "t2.nano"
+    tags = {
+      Name = "manasa"
+    }
+}
